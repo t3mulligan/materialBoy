5559c5e7486c0d3523ee3507166f7e54972e5927
Tim Mulligan <t3.mulligan@gmail.com>
2014-12-15 13:08:44 -0500

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,27 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','ngMaterial'])
+
+    .controller('AppCtrl', function ($scope) {
+
+        $scope.form1 = {};
+        $scope.form1.zipBox="";
+
+    })
+
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
